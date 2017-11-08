f2bd83f4256e8f6382108eccea6b32d1289195de
Christopher Bloom <bloomcb@gmail.com>
2017-11-07 22:28:50 -0800

    F dev server done

--- pres/F/webpack.config.js
+++ pres/F/webpack.config.js
@@ -1,25 +1,43 @@
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './index.js',
+  entry: path.resolve(__dirname, 'index.js'),
   output: {
     filename: 'bundle.js',
-    path: __dirname
+    path: path.resolve(__dirname),
   },
   module: {
-    loaders: [
-      { test: /\.css$/, loader: "style-loader!css-loader" },
-      { test: /\.scss$/, loader: "style-loader!css-loader!sass-loader" }
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
     ]
   },
   devServer: {
-    colors: true,
     historyApiFallback: true,
     hot: true,
     inline: true,
-    progress: true
+    progress: true,
+    contentBase: path.resolve(__dirname),
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
   ]
 };
